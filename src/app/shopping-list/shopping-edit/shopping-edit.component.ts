afb6e0a3abda045fd4bb89e084f743e374a5caf4
Leo Matheus <leomraifur@gmail.com>
2023-12-04 15:13:24 -0300

    Finalizando o shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: false }) nameInputRef:
+    | ElementRef
+    | undefined;
+  @ViewChild('amountInput', { static: false }) amountInputRef:
+    | ElementRef
+    | undefined;
 
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem() {
+    const newIngredient = new Ingredient(
+      this.nameInputRef?.nativeElement.value,
+      this.amountInputRef?.nativeElement.value
+    );
+  }
 }
